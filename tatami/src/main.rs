0795021f9284f91ee83c88887ece60d2dd8672f7
Ruksi Korpisara <me@ruk.si>
2023-12-31 22:25:33 +0200

    ✨ Add hello world server

--- tatami/src/main.rs
+++ tatami/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{response::Html, routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(handler));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:10000")
+        .await
+        .unwrap();
+
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<h1>Hello, World!</h1>")
 }
