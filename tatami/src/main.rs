365f0822641c572ae22e1f57e8ccc3480b88dd8f
Ruksi Korpisara <me@ruk.si>
2024-01-01 01:52:12 +0200

    📒 Add tracing logging

--- tatami/src/main.rs
+++ tatami/src/main.rs
@@ -1,12 +1,19 @@
 use axum::{Router, routing::get};
 use axum::response::{Html, Redirect};
+use tokio::net::TcpListener;
 use tower_http::services::ServeDir;
+use tracing_subscriber::{EnvFilter, layer::SubscriberExt, util::SubscriberInitExt};
 
 use tatami::config::Config;
 
 #[tokio::main]
 async fn main() {
     let config = Config::load();
+
+    tracing_subscriber::registry()
+        .with(EnvFilter::try_from_default_env().unwrap_or_else(|_| "tatami=debug".into()))
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 
     let app = Router::new()
         // the path is relative to where the binary is run so under `ryokan/tatami` here
@@ -15,11 +22,12 @@ async fn main() {
         .route("/", get(index));
 
     let address = format!("0.0.0.0:{}", config.port);
-    let listener = tokio::net::TcpListener::bind(address)
+    let listener = TcpListener::bind(address)
         .await
         .unwrap();
 
-    println!("Listening on {} 📢", listener.local_addr().unwrap());
+    tracing::debug!("Listening on {} 📢", listener.local_addr().unwrap());
+
     axum::serve(listener, app).await.unwrap();
 }
 
