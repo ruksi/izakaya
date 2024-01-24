b8e0d9d89a1df38a912a7d41987e020d40a6f8c3
Ruksi Korpisara <me@ruk.si>
2024-01-24 18:59:02 +0200

    🦺✅ Add more validator tests

--- tatami/src/error/validation_error.rs
+++ tatami/src/error/validation_error.rs
@@ -46,4 +46,36 @@ mod tests {
         );
         Ok(())
     }
+
+    #[test]
+    fn validator_maintains_renamed_fields() -> Result<()> {
+        // Note: rename_all="camelCase" doesn't seem to work with validator :(
+        //       could fix that in the validator, but I don't want to work on that now
+        #[derive(Deserialize, Validate)]
+        struct Person {
+            #[validate(length(min = 3))]
+            #[serde(rename = "firstName")]
+            first_name: String,
+        }
+        let person = Person {
+            first_name: "bo".into(),
+        };
+        let err = person.validate().unwrap_err();
+        let text = serde_json::to_string(&err).unwrap();
+        let untyped = serde_json::from_str::<serde_json::Value>(&text).unwrap();
+        assert_eq!(
+            untyped,
+            json!({
+                "firstName": [{
+                    "code": "length",
+                    "message": null, // we actually drop nulls in our version
+                    "params": {
+                        "min": 3,
+                        "value": "bo",
+                    },
+                }],
+            })
+        );
+        Ok(())
+    }
 }
