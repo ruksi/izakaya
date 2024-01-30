eab2fcf22b21694983b6a540c1aace90259982ee
Ruksi Korpisara <me@ruk.si>
2024-01-30 07:36:32 +0200

    💕 Refactor backend health check
    Mainly so it cleans after itself.

--- tatami/src/endpoints/healthz.rs
+++ tatami/src/endpoints/healthz.rs
@@ -1,19 +1,32 @@
 use axum::extract::State;
-use redis::AsyncCommands;
 
 use crate::prelude::*;
 use crate::state::AppState;
 
+const HEALTH_CACHE_KEY: &str = "tatami:healthz";
+
 pub async fn healthz(State(state): State<AppState>) -> Result<String> {
-    let from_db: String = sqlx::query_scalar("SELECT 'DATABASE OK'")
+    let db_status = check_db(&state).await?;
+    let cache_status = check_cache(&state).await?;
+    Ok(format!("{}\n{}", db_status, cache_status))
+}
+
+pub async fn check_db(state: &AppState) -> Result<String> {
+    let db_status: String = sqlx::query_scalar("SELECT 'DATABASE OK'")
         .fetch_one(&state.db_pool)
         .await?;
+    Ok(db_status)
+}
 
-    let mut redis = state.cache_pool.get().await?;
-    redis.set("deadpool:test_key", "CACHE OK").await?;
-    let from_cache: String = redis.get("deadpool:test_key").await?;
-
-    Ok(format!("{}\n{}", from_db, from_cache))
+pub async fn check_cache(state: &AppState) -> Result<String> {
+    #[rustfmt::skip]
+    let (cache_status,): (String,) = redis::pipe()
+        .set(HEALTH_CACHE_KEY, "CACHE OK").ignore()
+        .get(HEALTH_CACHE_KEY)
+        .del(HEALTH_CACHE_KEY).ignore()
+        .query_async(&mut state.cache_pool.get().await?)
+        .await?;
+    Ok(cache_status)
 }
 
 #[cfg(test)]
@@ -25,10 +38,8 @@ mod tests {
     #[sqlx::test]
     async fn works(db: sqlx::PgPool) -> Result<()> {
         let server = mock_server(&db).await;
-        server
-            .get("/healthz")
-            .await
-            .assert_text("DATABASE OK\nCACHE OK");
+        let response = server.get("/healthz").await;
+        response.assert_text("DATABASE OK\nCACHE OK");
         Ok(())
     }
 }
