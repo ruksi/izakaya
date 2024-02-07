1f49868411b7eeaaa3db5f0ea9bba68f2dc39102
Ruksi Korpisara <me@ruk.si>
2024-02-07 05:07:34 +0200

    🍙 Read port from `PORT`

--- onigiri/vite.config.js
+++ onigiri/vite.config.js
@@ -1,7 +1,13 @@
-import {defineConfig} from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // noinspection JSUnusedGlobalSymbols
-export default defineConfig({
-    plugins: [react()],
+export default defineConfig(({mode}) => {
+    // eslint-disable-next-line no-undef
+    const env = loadEnv(mode, process.cwd(), '');
+    const port = env.PORT || 5173;
+    return {
+        server: {port},
+        plugins: [react()]
+    };
 })
