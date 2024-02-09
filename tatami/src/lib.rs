7ad71d9d6c38d26bb7758ddc89f37bb8b52c601e
Ruksi Korpisara <me@ruk.si>
2024-02-09 06:52:01 +0200

    🟧🚚 Move the whole app definition to `lib`

--- tatami/src/lib.rs
+++ tatami/src/lib.rs
@@ -1,10 +1,12 @@
 use std::time::Duration;
 
 use axum::extract::{MatchedPath, Request};
+use axum::http::{header, HeaderValue, Method};
 use axum::response::Redirect;
 use axum::routing::get;
 use axum::Router;
 use tokio::time::sleep;
+use tower_http::cors::CorsLayer;
 use tower_http::services::ServeDir;
 use tower_http::trace::TraceLayer;
 
@@ -24,7 +26,7 @@ mod valid;
 #[cfg(test)]
 mod test_utils;
 
-pub async fn get_app<S>(config: &Config) -> Router<S> {
+pub async fn get_app<S: Clone + Send + Sync + 'static>(config: Config) -> Router<S> {
     // Railway private networks take time to initialize on deployment,
     // and application crashes make it re-initialize, so we have to wait
     // https://docs.railway.app/reference/private-networking#caveats
@@ -54,7 +56,37 @@ pub async fn get_app<S>(config: &Config) -> Router<S> {
     let cookie_secret = auth::cookie::cookie_secret_from_seed(config.secret_key.clone());
 
     let state = AppState::new(db_pool, cache_pool, cookie_secret);
-    root_router(state)
+    let mut app = root_router(state);
+
+    let mut origins: Vec<HeaderValue> = vec![];
+    if let Some(frontend_urls) = config.frontend_urls {
+        for frontend_url in frontend_urls {
+            origins.push(frontend_url.parse().unwrap());
+        }
+    }
+    if !origins.is_empty() {
+        app = app.layer(
+            CorsLayer::new()
+                .allow_headers([
+                    header::CONTENT_TYPE,
+                    header::CONTENT_LENGTH,
+                    header::CONTENT_LANGUAGE,
+                ])
+                .allow_methods([
+                    Method::GET,
+                    Method::POST,
+                    Method::PUT,
+                    Method::PATCH,
+                    Method::DELETE,
+                    Method::HEAD,
+                    Method::OPTIONS,
+                ])
+                .allow_credentials(true) // allow frontend to send us cookies
+                .allow_origin(origins),
+        );
+    }
+
+    app
 }
 
 fn root_router<S>(state: AppState) -> Router<S> {
--- tatami/src/main.rs
+++ tatami/src/main.rs
@@ -1,7 +1,5 @@
-use axum::http::{header, HeaderValue, Method};
 use tokio::net::TcpListener;
-use tower_http::cors::CorsLayer;
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
+use tracing_subscriber::{EnvFilter, layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() {
@@ -16,39 +14,7 @@ async fn main() {
         .init();
 
     let listener = TcpListener::bind(config.bind_address()).await.unwrap();
-
-    let mut app = tatami::get_app(&config).await;
-
-    let mut origins: Vec<HeaderValue> = vec![];
-    if let Some(frontend_urls) = config.frontend_urls {
-        // TODO: this multiple allowed origins smells fishy, do all browsers support this?
-        for frontend_url in frontend_urls {
-            origins.push(frontend_url.parse().unwrap());
-        }
-    }
-    if !origins.is_empty() {
-        app = app.layer(
-            CorsLayer::new()
-                .allow_headers([
-                    header::CONTENT_TYPE,
-                    header::CONTENT_LENGTH,
-                    header::CONTENT_LANGUAGE,
-                ])
-                .allow_methods([
-                    Method::GET,
-                    Method::POST,
-                    Method::PUT,
-                    Method::PATCH,
-                    Method::DELETE,
-                    Method::HEAD,
-                    Method::OPTIONS,
-                ])
-                .allow_credentials(true) // allow frontend to send us cookies
-                .allow_origin(origins),
-        );
-    }
-
-    // todo: https://docs.rs/axum/latest/axum/middleware/index.html#commonly-used-middleware
+    let app = tatami::get_app(config).await;
 
     tracing::debug!("Listening on {} 📢", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
