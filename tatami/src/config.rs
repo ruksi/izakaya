c03675c469c1b9758e5c828cc363157108ba3026
Ruksi Korpisara <me@ruk.si>
2024-02-09 06:52:03 +0200

    🟧🔧 Make default config values constants

--- tatami/src/config.rs
+++ tatami/src/config.rs
@@ -1,22 +1,22 @@
+const DEFAULT_PORT: &str = "8080";
+const DEFAULT_RUST_LOG: &str = "tatami=debug";
+
 pub struct Config {
     pub port: String,
     pub rust_log: String,
     pub database_url: String, // aka. PostgreSQL
     pub cache_url: String,    // aka. Redis
-    pub secret_key: String, // a generic seed (64+ character string) used for hashes, salts, and the like
+    pub secret_key: String,   // a generic seed (64+ character string) used for hashes, salts, and the like
     pub frontend_urls: Option<Vec<String>>,
 }
 
 impl Config {
     pub fn load() -> Self {
-        let port = std::env::var("PORT").unwrap_or_else(|_| "8080".to_string());
-
-        let rust_log = std::env::var("RUST_LOG").unwrap_or_else(|_| "tatami=debug".to_string());
+        let port = std::env::var("PORT").unwrap_or_else(|_| DEFAULT_PORT.to_string());
+        let rust_log = std::env::var("RUST_LOG").unwrap_or_else(|_| DEFAULT_RUST_LOG.to_string());
 
         let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-
         let cache_url = std::env::var("CACHE_URL").expect("CACHE_URL must be set");
-
         let secret_key = std::env::var("SECRET_KEY").expect("SECRET_KEY must be set");
 
         let frontend_urls = std::env::var("FRONTEND_URL").ok().map(split_urls);
