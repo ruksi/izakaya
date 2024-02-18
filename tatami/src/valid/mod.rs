35efb677f41c5424c9ca38d43e1f8bef8763850d
Ruksi Korpisara <me@ruk.si>
2024-02-18 06:46:35 +0200

    🟧 Forbid creating Valids without validation

--- tatami/src/valid/mod.rs
+++ tatami/src/valid/mod.rs
@@ -5,3 +5,40 @@ pub use valid::Valid;
 mod password;
 mod username;
 mod valid;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::prelude::*;
+    use validator::Validate;
+
+    #[tokio::test]
+    async fn valid_scope() -> Result<()> {
+        #[derive(Validate)]
+        struct Person {
+            #[validate(length(min = 1, max = 10))]
+            name: String,
+        }
+        let person = Person {
+            name: "John".to_string(),
+        };
+        let valid_person = Valid::new(person)?;
+
+        // you can get a reference to the inner value, but immutable only
+        assert_eq!(valid_person.inner_as_ref().name, "John");
+
+        // you can consume the inner value
+        let person2 = valid_person.into_inner();
+        assert_eq!(person2.name, "John");
+
+        // you can't skip the validation step, the inner struct is private,
+        // e.g. the next won't compile:
+        // Valid {
+        //     inner: ValidInner(MyJson {
+        //         name: "John".to_string(),
+        //     }),
+        // };
+
+        Ok(())
+    }
+}
--- tatami/src/valid/valid.rs
+++ tatami/src/valid/valid.rs
@@ -3,39 +3,43 @@ use axum::extract::FromRequest;
 use axum::{async_trait, Json};
 use validator::Validate;
 
-use crate::error;
-use crate::error::Error;
+use crate::prelude::*;
 
-// Valid a newtype that marks that the inner, JSON deserializable value
-// has been validated, and the only way to mutate the inner value is to
-// remove the validated marker (no Arc, no RefCell, no Mutex, no RwLock, please 🙏).
+// Valid a typestate that marks that the contained struct has been validated, and
+// the only way to mutate the inner value is to remove the validated marker
+// (don't use else with multiple ownership 🙏).
 //
-// You should be using it in the request handlers through the `Valid<T>` type like:
-// * `Valid(my_json): Valid<MyJson>`
-// * or, if you want to keep the validated marker: `my_json: Valid<MyJson>`
-//
-// This makes sure that the value both 1) is valid JSON and 2) passes Validator validation
+// You can use it in Axum request handlers through `Valid<T>` like:
+// * `Valid(person): Valid<Person>`
+// * or, if you want to keep the validated marker: `person: Valid<Person>`
 
-#[derive(Debug, Clone, Copy, Default)]
-pub struct Valid<T>(T)
-where
-    T: serde::de::DeserializeOwned + Validate;
+pub struct Valid<T: Validate> {
+    inner: ValidInner<T>,
+}
 
-impl<T> Valid<T>
-where
-    T: serde::de::DeserializeOwned + Validate,
-{
-    pub fn new(value: T) -> error::Result<Self> {
-        value.validate()?;
-        Ok(Self(value))
-    }
-    pub fn inner_as_ref(&self) -> &T {
-        &self.0 // "aka. 'I just want to have a peek.'"
-    }
-    pub fn into_inner(self) -> T {
-        self.0 // "aka. 'I want to consume this thing now, please.'"
+impl<T: Validate + std::fmt::Debug> std::fmt::Debug for Valid<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_tuple("Valid").field(self.inner_as_ref()).finish()
     }
 }
+
+impl<T: Validate> Valid<T> {
+    pub fn new(value: T) -> Result<Self> {
+        value.validate()?;
+        Ok(Self {
+            inner: ValidInner(value),
+        })
+    }
+    pub fn inner_as_ref(&self) -> &T {
+        &self.inner.0 // "aka. 'I just want to have a peek.'"
+    }
+    pub fn into_inner(self) -> T {
+        self.inner.0 // "aka. 'I want to consume this thing now, please.'"
+    }
+}
+
+// private scope, so that the only way to create a `Valid<T>` is through `Valid::new`
+struct ValidInner<T: Validate>(T);
 
 #[async_trait]
 impl<T, S> FromRequest<S> for Valid<T>
@@ -46,7 +50,7 @@ where
 {
     type Rejection = Error;
 
-    async fn from_request(req: axum::extract::Request, state: &S) -> error::Result<Self> {
+    async fn from_request(req: axum::extract::Request, state: &S) -> Result<Self> {
         let Json(thing_from_json) = Json::<T>::from_request(req, state).await?;
         let valid_thing_from_json = Valid::new(thing_from_json)?;
         Ok(valid_thing_from_json)
