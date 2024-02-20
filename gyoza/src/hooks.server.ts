a2513aec9fdc52b7ec6cf0bfdc4fd2554ef314d9
Ruksi Korpisara <me@ruk.si>
2024-02-21 00:05:07 +0200

    🥟 Allow SvelteKit backend to pass user cookies

--- gyoza/src/hooks.server.ts
+++ gyoza/src/hooks.server.ts
@@ -1,13 +1,16 @@
-// TODO: not sure we really need this yet
-// import type {HandleFetch} from "@sveltejs/kit";
-//
-// export const handleFetch: HandleFetch = async ({event, request, fetch}) => {
-//     if (request.url.startsWith("http://localhost:8080")) {
-//         // pass user cookies from SvelteKit backend to the API backend
-//         const cookies = event.request.headers.get("Cookie");
-//         if (cookies) {
-//             request.headers.set("Cookie", cookies);
-//         }
-//     }
-//     return fetch(request);
-// };
+import {backendUrl} from "$lib/utils";
+import type {HandleFetch} from "@sveltejs/kit";
+
+const baseUrl = backendUrl();
+
+export const handleFetch: HandleFetch = async ({event, request, fetch}) => {
+    // TODO: is it bad that this can cause some cookies to be send to ether? 😅
+    if (request.url.startsWith(baseUrl)) {
+        // pass user cookies from SvelteKit backend to the API backend
+        const cookies = event.request.headers.get("Cookie");
+        if (cookies) {
+            request.headers.set("Cookie", cookies);
+        }
+    }
+    return fetch(request);
+};
