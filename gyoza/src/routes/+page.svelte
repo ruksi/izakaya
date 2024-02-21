8c6265b1ff9f60084e8f0aee02e6665812490f41
Ruksi Korpisara <me@ruk.si>
2024-02-21 07:37:06 +0200

    🥟 Update landing page

--- gyoza/src/routes/+page.svelte
+++ gyoza/src/routes/+page.svelte
@@ -1,8 +1,33 @@
-<div class="prose prose-invert prose-zinc">
-    <h1>Number One</h1>
-    <h2>Number Two</h2>
-    <h3>Number Three</h3>
-    <h4>Number Four</h4>
-    <h5>Number Five</h5>
-    <h6>Number Six</h6>
-</div>
+<main class="flex flex-col max-w-7xl mx-auto p-4">
+    <div class="pt-40 pb-20">
+        <div class="flex">
+            <span class="self-center px-4">xxx</span>
+            <h1 class="text-6xl font-bold">This is Izakaya landing page</h1>
+        </div>
+        <div class="text-2xl text-zinc-500">with Vite + SvelteKit + TanStack Query</div>
+    </div>
+
+    <div class="py-20 flex justify-center">
+        <div class="flex flex-col text-center">
+            <div class="text-xl">Social proof is excellent for assurance.</div>
+            <div class="text-sm text-zinc-400">— Bob Bobbins in <cite>the Big Company</cite></div>
+        </div>
+    </div>
+
+    <section class="py-20 cyan">
+        <header>Core Message</header>
+        <p>
+            This is why you should use this service or product, and how it will change your life
+            forever.
+        </p>
+    </section>
+</main>
+
+<style>
+    section {
+        @apply text-5xl leading-tight font-light;
+    }
+    section.cyan header {
+        @apply text-cyan-400 mb-5;
+    }
+</style>
